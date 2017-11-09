7cf2ce1741e84609483f814f5462f67a2235fa47
Pierre_T <pierre.tsiakkaros@gmail.com>
2017-11-09 19:35:26 +0100

    modif kids to object of objects

--- client/app/models/kidmodels.ts
+++ client/app/models/kidmodels.ts
@@ -1,68 +1,68 @@
-export const KIDS: any[] = [
-    {
+export const KIDS = {
+    p : {
         firstName: "Paul",
         lastName: "Cailly",
         photoUrl: "./assets/img/pp/paul.jpg",
         sage : 8,
         customMsg : "Tu as bien travaillé cette année, mon petit Paul. Mais vas-y mollo sur le RedBull quand même !"
     },  // 0
-    {
+    j: {
         firstName: "Jeremy",
         lastName: "Perrin",
         photoUrl: "./assets/img/pp/jeremy.jpg",
         
     }, //1
-    {
+    a: {
         firstName: "Alexandra",
         lastName: "Maquet",
         photoUrl: "./assets/img/pp/alexandra.jpg"
     }, //2
-    {
+    pp: {
         firstName: "Pierre",
         lastName: "Gorce",
         photoUrl: "./assets/img/pp/pierreg.jpg"
     },//3
-    {
+    r: {
         firstName: "Robert",
         lastName: "Schmid",
         photoUrl: "./assets/img/pp/robert.jpg"
     },//4
-    {
+    s: {
         firstName: "Sylvain",
         lastName: "Le Crom",
         photoUrl: "./assets/img/pp/sylvain.jpg"
     },//5
-    {
+    t: {
         firstName: "Thomas",
         lastName: "Godinot",
         photoUrl: "./assets/img/pp/unknown.jpg"
     },//6
-    {
+    v: {
 
         firstName: "Victor",
         lastName: "Leduc",
         photoUrl: "./assets/img/pp/victor.jpg"
     },//7
-    {
+    m: {
         firstName: "Maxime",
         lastName: "Cornuaud",
         photoUrl: "./assets/img/pp/maxime.jpg"
     },//8 
-    {
+    ppp : {
         firstName: "Pierre",
         lastName: "Tsiakkaros",
         photoUrl: "./assets/img/pp/pierret.jpg"
     },//9
-    {
+    aa : {
         firstName: "Anna",
         lastName: "Stepanoff",
         photoUrl: "./assets/img/pp/anna.jpeg"
     },//10
 
-    {
+   mm: {
         firstName: "Matthieu",
         lastName: "Marais",
         photoUrl: "./assets/img/pp/matthieu.jpg"
     }//11
 
-]
+}
