16527ffb32988de43f9be19b332cd528df21cdba
Pierre_T <pierre.tsiakkaros@gmail.com>
2017-11-10 07:41:55 +0100

    values dans kidService

--- client/app/services/kid.service.ts
+++ client/app/services/kid.service.ts
@@ -3,11 +3,15 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class KidService {
   scanned = false;
+  ;
+  values = '';
   constructor() { }
 
   scanMethod(){
 
         this.scanned = true;
+        console.log(this.values)
+        
         
     
   }
