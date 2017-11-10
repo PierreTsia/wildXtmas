7ee6c4614cc12a699e3b3e8398d016ff9b3146fb
Jeremy Perrin <jeremy.perrin09@gmail.com>
2017-11-10 01:32:00 +0100

    ajout gps santa

--- client/app/models/kidmodels.ts
+++ client/app/models/kidmodels.ts
@@ -1,68 +1,77 @@
 export const KIDS = {
-    p : {
+    'p' : {
         firstName: "Paul",
         lastName: "Cailly",
         photoUrl: "./assets/img/pp/paul.jpg",
-        sage : 8,
-        customMsg : "Tu as bien travaillé cette année, mon petit Paul. Mais vas-y mollo sur le RedBull quand même !"
+        address : "Fontaine-Simon"
     },  // 0
-    j: {
+    'j': {
         firstName: "Jeremy",
         lastName: "Perrin",
         photoUrl: "./assets/img/pp/jeremy.jpg",
-        
+        address : "St-Eliph"
     }, //1
-    a: {
+    'a': {
         firstName: "Alexandra",
         lastName: "Maquet",
-        photoUrl: "./assets/img/pp/alexandra.jpg"
+        photoUrl: "./assets/img/pp/alexandra.jpg",
+        address : "Chartres"
     }, //2
     pp: {
         firstName: "Pierre",
         lastName: "Gorce",
-        photoUrl: "./assets/img/pp/pierreg.jpg"
+        photoUrl: "./assets/img/pp/pierreg.jpg",
+        address : "Le Pas Saint l'Homer"
     },//3
     r: {
         firstName: "Robert",
         lastName: "Schmid",
-        photoUrl: "./assets/img/pp/robert.jpg"
+        photoUrl: "./assets/img/pp/robert.jpg",
+        address : "Bonneval"
     },//4
-    s: {
+    's': {
         firstName: "Sylvain",
         lastName: "Le Crom",
-        photoUrl: "./assets/img/pp/sylvain.jpg"
+        photoUrl: "./assets/img/pp/sylvain.jpg",
+        address : "Rouen"
     },//5
     t: {
         firstName: "Thomas",
         lastName: "Godinot",
-        photoUrl: "./assets/img/pp/unknown.jpg"
+        photoUrl: "./assets/img/pp/unknown.jpg",
+        address : "Chartres"
     },//6
     v: {
 
         firstName: "Victor",
         lastName: "Leduc",
-        photoUrl: "./assets/img/pp/victor.jpg"
+        photoUrl: "./assets/img/pp/victor.jpg",
+        address : "Remalard"
     },//7
     m: {
         firstName: "Maxime",
         lastName: "Cornuaud",
-        photoUrl: "./assets/img/pp/maxime.jpg"
+        photoUrl: "./assets/img/pp/maxime.jpg",
+        address :"La Loupe"
     },//8 
     ppp : {
         firstName: "Pierre",
         lastName: "Tsiakkaros",
-        photoUrl: "./assets/img/pp/pierret.jpg"
+        photoUrl: "./assets/img/pp/pierret.jpg",
+        address : "Paris"
     },//9
     aa : {
         firstName: "Anna",
         lastName: "Stepanoff",
-        photoUrl: "./assets/img/pp/anna.jpeg"
+        photoUrl: "./assets/img/pp/anna.jpeg",
+        address: "St Eliph"
     },//10
 
    mm: {
         firstName: "Matthieu",
         lastName: "Marais",
-        photoUrl: "./assets/img/pp/matthieu.jpg"
+        photoUrl: "./assets/img/pp/matthieu.jpg",
+        address: "Belhomert"
     }//11
 
 }
